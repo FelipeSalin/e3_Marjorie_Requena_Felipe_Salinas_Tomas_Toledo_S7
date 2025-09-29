07c2577f511a1f7c07ebb9064a3e945e4afe9a39
Rokurook <ma.requena@duocuc.cl>
2025-09-29 01:02:03 -0300

    agregamos Serializer /maraca -detalle

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,8 +1,23 @@
-#from rest_framework import serializers
+from rest_framework import serializers
 #ejemplo
-#from core.models import Mascota
+from Inicio.models import TipoUsuario, Usuario, Comuna, Region, Direccion, Venta, Categoria, TipoProd, Marca, Modelo, Producto, Detalle 
 
-#class MascotaSerializer(serializers.ModelSerializer):
-    #class Meta:
-        #model = Mascota
-        #fields = ['codigoChip', 'nombreMascota', 'edadMascota', 'foto', 'raza'] 
+class MarcaSerializer(serializers.ModeloSerializer):
+    class Marca:
+        model = Marca
+        fields = ['idMarca', 'nombreMarca']
+        
+class ModeloSerializer(serializers.ModeloSerializer):
+    class Modelo:
+        model = Modelo
+        fields = ['idModelo', 'nombreModelo', 'Marca']
+        
+class ProductoSerializer(serializers.ModeloSerializer):
+    class Producto:
+        model = Producto
+        fields = ['idProducto', 'nombreProducto', 'precioProducto','especificacionProd', 'stockProd','imagenProd', 'tipoprod', 'marca' ]
+        
+class DetalleSerializer(serializers.ModeloSerializer):
+    class Detalle:
+        model = Detalle
+        fields = ['idDetalle', 'cantidad ', 'subTotal', 'venta', 'producto']
