b332fac2f031c661d7b31a6cb135e841bf3a444e
Squawks1 <to.toledom@duocuc.cl>
2025-09-29 01:06:28 -0300

    update serializers.py
    se agregan 4 modelos más

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 #ejemplo
 from Inicio.models import TipoUsuario, Usuario, Comuna, Region, Direccion, Venta, Categoria, TipoProd, Marca, Modelo, Producto, Detalle 
 
+class tipoUsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoUsuario
+        fields = ['idTipoUsuario', 'nombreTipo']
+
+class usuarioSerializar(serializers.Serializer):
+    class Meta:
+        model = Usuario
+        fields = ['username', 'contrasennia', 'nombre', 'apellido', 'email', 'tipousuario']
+
+class comunaSerializer(serializers.Serializer):
+    class Meta:
+        model = Comuna
+        fields = ['idComuna', 'nombreCom']
+
+class regionSerializer(serializers.Serializer):
+    class Meta:
+        model = Region
+        fields = ['idRegion', 'nombreReg', 'comuna']
+
 class MarcaSerializer(serializers.ModeloSerializer):
     class Marca:
         model = Marca
