ce9da3951fa472f883f0cab0256d9292e8dd426d
Felipe Salinas <fea.salinas@duocuc.cl>
2025-09-28 23:38:47 -0300

    Wallet integrado en manage.py, migración hecha

--- manage.py
+++ manage.py
@@ -2,7 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import oracledb
 
+oracledb.init_oracle_client(lib_dir=r'C:\Users\rlla2\OneDrive\Escritorio\Analista Programador Computacional\DUOC\Bimestre 5\Programacion Web\InstantClient\instantclient_23_9')
+
+os.environ['TNS_ADMIN'] = r'C:\Users\rlla2\OneDrive\Escritorio\Analista Programador Computacional\DUOC\Bimestre 5\Programacion Web\Wallet'
 
 def main():
     """Run administrative tasks."""
